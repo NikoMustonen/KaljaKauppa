a26b7596161abc6904d6b609d56a4f035a21f752
Niko Mustonen <mustonen.niko@gmail.com>
2017-03-30 13:33:31 +0300

    Add component scanning

--- src/main/java/fi/tamk/beerbros/kaljakauppa/app/Main.java
+++ src/main/java/fi/tamk/beerbros/kaljakauppa/app/Main.java
@@ -2,8 +2,10 @@ package fi.tamk.beerbros.kaljakauppa.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"fi.tamk.beerbros.kaljakauppa.rest"})
 public class Main {
 
     public static void main(String... args) {
