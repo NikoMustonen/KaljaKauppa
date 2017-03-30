03390897dd8cfbd047563d3764af24e2ab1b6287
Niko Mustonen <mustonen.niko@gmail.com>
2017-03-30 13:28:00 +0300

    Add entity properties

--- src/main/java/fi/tamk/beerbros/kaljakauppa/database/entities/Region.java
+++ src/main/java/fi/tamk/beerbros/kaljakauppa/database/entities/Region.java
@@ -1,5 +1,37 @@
 package fi.tamk.beerbros.kaljakauppa.database.entities;
 
-public class Region {
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+public class Region implements Serializable {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private String name;
+    
+    public Region(){}
+    
+    public Region(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
