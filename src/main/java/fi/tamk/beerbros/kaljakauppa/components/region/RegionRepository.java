9895829670f77f0ded4a6841853dbf5036ea1c6e
Niko Mustonen <mustonen.niko@gmail.com>
2017-03-30 16:51:58 +0300

    Create RegionRepository interface for region database entities

---
+++ src/main/java/fi/tamk/beerbros/kaljakauppa/components/region/RegionRepository.java
@@ -0,0 +1,5 @@
+package fi.tamk.beerbros.kaljakauppa.components.region;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RegionRepository extends CrudRepository<Region, Integer>{}
