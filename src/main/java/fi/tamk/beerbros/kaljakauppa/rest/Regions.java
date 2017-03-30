efa0f8e16f0a9990dfa71c0eece83fcc13b19385
Niko Mustonen <mustonen.niko@gmail.com>
2017-03-30 13:33:12 +0300

    Create base point for getting all the regions from the database

--- src/main/java/fi/tamk/beerbros/kaljakauppa/rest/Regions.java
+++ src/main/java/fi/tamk/beerbros/kaljakauppa/rest/Regions.java
@@ -1,9 +1,13 @@
 package fi.tamk.beerbros.kaljakauppa.rest;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class Regions {
     
-    
+    @RequestMapping("/regions")
+    public String getAllRegions() {
+        return "all regions";
+    }
 }
