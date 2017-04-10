a699282fdf7dcd3a37ce106e7d22f8426a40c38d
Niko Mustonen <mustonen.niko@gmail.com>
2017-04-10 14:25:50 +0300

    Redefine project structure and create beer adding

--- src/main/java/fi/tamk/beerbros/kaljakauppa/components/country/CountryRepository.java
+++
@@ -1,5 +0,0 @@
-package fi.tamk.beerbros.kaljakauppa.components.country;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CountryRepository extends CrudRepository<Country, String>{}
--- src/main/java/fi/tamk/beerbros/kaljakauppa/components/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package fi.tamk.beerbros.kaljakauppa.components.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    
-}
