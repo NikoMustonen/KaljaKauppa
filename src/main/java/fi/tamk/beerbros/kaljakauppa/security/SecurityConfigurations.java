c6c6e3dde158befe65c8a9d98c085c680c56cf0f
Niko Mustonen <mustonen.niko@gmail.com>
2017-04-26 17:16:33 +0300

    Clean imports

--- src/main/java/fi/tamk/beerbros/kaljakauppa/security/SecurityConfigurations.java
+++ src/main/java/fi/tamk/beerbros/kaljakauppa/security/SecurityConfigurations.java
@@ -1,8 +1,34 @@
 package fi.tamk.beerbros.kaljakauppa.security;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.*;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.web.authentication.*;
 
+@EnableWebSecurity
+@Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
-    
-    
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new LoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new AuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("password")
+                .roles("ADMIN");
+    }
 }
