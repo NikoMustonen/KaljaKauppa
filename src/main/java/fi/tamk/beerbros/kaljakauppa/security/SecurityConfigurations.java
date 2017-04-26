89d73d27a55b4ac217c854513ff60bc160d6f625
Niko Mustonen <mustonen.niko@gmail.com>
2017-04-26 16:45:37 +0300

    Create configuration class for security

---
+++ src/main/java/fi/tamk/beerbros/kaljakauppa/security/SecurityConfigurations.java
@@ -0,0 +1,8 @@
+package fi.tamk.beerbros.kaljakauppa.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+    
+    
+}
