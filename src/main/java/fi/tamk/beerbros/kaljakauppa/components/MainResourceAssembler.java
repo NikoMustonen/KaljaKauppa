2966ae48fa7779618d3db98c3d5152a1455d8c18
Niko Mustonen <mustonen.niko@gmail.com>
2017-04-26 23:36:49 +0300

    Add HATEOAS routes to manufacturers and users

--- src/main/java/fi/tamk/beerbros/kaljakauppa/components/MainResourceAssembler.java
+++ src/main/java/fi/tamk/beerbros/kaljakauppa/components/MainResourceAssembler.java
@@ -3,6 +3,8 @@ package fi.tamk.beerbros.kaljakauppa.components;
 import fi.tamk.beerbros.kaljakauppa.components.beer.BeerController;
 import fi.tamk.beerbros.kaljakauppa.components.beertype.BeerTypeController;
 import fi.tamk.beerbros.kaljakauppa.components.manufacturer.ManufacturerController;
+import fi.tamk.beerbros.kaljakauppa.components.country.CountryController;
+import fi.tamk.beerbros.kaljakauppa.components.user.UserController;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
@@ -17,8 +19,9 @@ public class MainResourceAssembler
         Resource<Object> resource = new Resource<>(t);
         resource.add(linkTo(BeerController.class).withRel("beers"));
         resource.add(linkTo(BeerTypeController.class).withRel("beertypes"));
-        resource.add(linkTo(ManufacturerController.class).withRel("countries"));
+        resource.add(linkTo(CountryController.class).withRel("countries"));
+        resource.add(linkTo(ManufacturerController.class).withRel("manufacturers"));
+        resource.add(linkTo(UserController.class).withRel("users"));
         return resource;
     }
-
 }
