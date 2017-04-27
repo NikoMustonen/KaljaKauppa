1cb5e4d19a4ce74288a4a421806baf45a2ad3d51
Niko Mustonen <mustonen.niko@gmail.com>
2017-04-27 04:34:11 +0300

    Create class for database access

---
+++ src/main/java/fi/tamk/beerbros/kaljakauppa/components/review/ReviewRepository.java
@@ -0,0 +1,7 @@
+package fi.tamk.beerbros.kaljakauppa.components.review;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReviewRepository
+        extends CrudRepository<Review, Long> {
+}
