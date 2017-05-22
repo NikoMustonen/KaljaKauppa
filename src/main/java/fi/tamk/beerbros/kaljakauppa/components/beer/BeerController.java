de31db4a1077c7c90b2e952fbd71d9e40ccdcd1d
Niko Mustonen <mustonen.niko@gmail.com>
2017-05-22 21:56:05 +0300

    Add beer filtering

--- src/main/java/fi/tamk/beerbros/kaljakauppa/components/beer/BeerController.java
+++ src/main/java/fi/tamk/beerbros/kaljakauppa/components/beer/BeerController.java
@@ -1,5 +1,9 @@
 package fi.tamk.beerbros.kaljakauppa.components.beer;
 
+import com.google.common.collect.Lists;
+import fi.tamk.beerbros.kaljakauppa.components.beertype.BeerType;
+import fi.tamk.beerbros.kaljakauppa.components.country.Country;
+import fi.tamk.beerbros.kaljakauppa.components.manufacturer.Manufacturer;
 import fi.tamk.beerbros.kaljakauppa.components.review.Review;
 import fi.tamk.beerbros.kaljakauppa.components.review.ReviewResourceAssembler;
 import java.sql.Timestamp;
@@ -17,80 +21,130 @@ public class BeerController {
 
     @Autowired
     private BeerRepository br;
-    
+
     @Autowired
     BeerResourceAssembler resourceAssembler;
-    
+
     @Autowired
     ReviewResourceAssembler reviewResourceAssembler;
-    
+
     @RequestMapping(
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Resources<Resource<Beer>> getBeers() {
+    public Resources<Resource<Beer>> getBeers(
+            @RequestParam(value = "beertype", required = false) String beertype,
+            @RequestParam(value = "manufacturer", required = false) String manufacturer,
+            @RequestParam(value = "country", required = false) String country,
+            @RequestParam(value = "start", required = false) Integer start,
+            @RequestParam(value = "step", required = false) Integer step) {
+
+        List<Beer> beers;
+        Manufacturer m = (manufacturer == null) ? null : new Manufacturer(manufacturer);
+        Country c = (country == null) ? null : new Country(country);
+        BeerType bt = (beertype == null) ? null : new BeerType(beertype);
         
-        Iterable<Beer> beers = br.findAll();
-        List<Resource<Beer>> beerResourceList = new ArrayList<>();
-        
-        for(Beer b : beers) {
-            b.setReviews(null);
-            beerResourceList.add(resourceAssembler.toResource(b));
+        if(m != null && c != null && bt != null) {
+            beers = br.findByCountryAndManufacturerAndBeerType(c, m, bt);
+        } else if(m == null && c != null && bt != null) {
+            beers = br.findByBeerTypeAndCountry(bt, c);
+        } else if(m == null && c == null && bt != null) {
+            beers = br.findByBeerType(bt);
+        } else if(m == null && c != null && bt != null) {
+            beers = br.findByCountry(c);
+        } else if(m != null && c == null && bt != null) {
+            beers = br.findByBeerTypeAndManufacturer(bt, m);
+        } else if(m != null && c == null && bt == null) {
+            beers = br.findByManufacturer(m);
+        } else if(m != null && c != null && bt == null) {
+            beers = br.findByCountryAndManufacturer(c, m);
+        } else {
+            beers = Lists.newArrayList(br.findAll());
         }
-        
-        return new Resources<>(beerResourceList, linkTo(BeerController.class).withSelfRel());
+            
+        List<Resource<Beer>> beerResourceList = new ArrayList<>();
+
+        if (start != null && start >= 0 && step != null && start + step < beers.size()) {
+            for (int i = start - 1; i < start - 1 + step; i++) {
+                beers.get(i).setReviews(null);
+                beerResourceList.add(resourceAssembler.toResource(beers.get(i)));
+            }
+        } else {
+            for (Beer b : beers) {
+                b.setReviews(null);
+                beerResourceList.add(resourceAssembler.toResource(b));
+            }
+        }
+        Resources<Resource<Beer>> resources
+                = new Resources<>(beerResourceList,
+                        linkTo(BeerController.class).withSelfRel());
+        if (start != null && start >= 1 && step != null && step > 0) {
+
+            if (start > 1) {
+                int first = (start - step > 1) ? start - step : 1;
+                resources.add(linkTo(BeerController.class).slash("?start=" + first + "&step=" + step).withRel("previous"));
+            }
+
+            int nextFirst = start + step;
+            int nextLast = start + step + step;
+            if (nextLast < beers.size()) {
+                resources.add(linkTo(BeerController.class).slash("?start=" + nextFirst + "&step=" + step).withRel("next"));
+            }
+        }
+
+        return resources;
     }
-    
+
     @RequestMapping(
             value = "/{id}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Resource<Beer> getBeer(@PathVariable int id) {
-        
+
         Beer b = br.findOne(id);
-        
+
         b.setReviews(null);
-        
+
         return this.resourceAssembler.toResource(b);
     }
-    
+
     @RequestMapping(
             value = "/{id}/reviews/{reviewId}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Resource<Review> getReviewByBeerAndId(
             @PathVariable int id, @PathVariable long reviewId) {
-        
+
         Beer b = br.findOne(id);
-        
-        for(Review r : b.getReviews()) {
-            if(r.getId() == reviewId) {
+
+        for (Review r : b.getReviews()) {
+            if (r.getId() == reviewId) {
                 r.getBeer().setReviews(null);
                 return reviewResourceAssembler.toResource(r);
             }
         }
-        
+
         return null;
     }
-    
+
     @RequestMapping(
             value = "/{id}/reviews",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Resources<Resource<Review>> getReviewsByBeer(@PathVariable int id) {
-        
+
         Beer b = br.findOne(id);
         List<Resource<Review>> reviewsByBeer = new ArrayList<>();
-        
-        for(Review r : b.getReviews()) {
+
+        for (Review r : b.getReviews()) {
             reviewsByBeer.add(reviewResourceAssembler.toResource(r));
             r.setBeerNull();
         }
-        
-        return new Resources<>(reviewsByBeer, 
-                linkTo(BeerController.class).slash(id).slash("reviews").withSelfRel()
-                ,linkTo(BeerController.class).slash(id).withRel("beer"));
+
+        return new Resources<>(reviewsByBeer,
+                linkTo(BeerController.class).slash(id).slash("reviews").withSelfRel(),
+                 linkTo(BeerController.class).slash(id).withRel("beer"));
     }
-    
+
     @RequestMapping(
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -100,7 +154,7 @@ public class BeerController {
         beer.setTimeAdded(new Timestamp(System.currentTimeMillis()));
         br.save(beer);
     }
-    
+
     @RequestMapping(
             value = "/{id}",
             method = RequestMethod.PUT,
@@ -108,16 +162,16 @@ public class BeerController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public Resource<Beer> modifyBeer(
-            @PathVariable int id, 
+            @PathVariable int id,
             @RequestBody Beer beer) {
         Beer b = br.findOne(id);
-        if(b != null) {
+        if (b != null) {
             b = beer;
             b.setId(id);
         }
         return resourceAssembler.toResource(br.save(b));
     }
-    
+
     @RequestMapping(
             value = "/{id}",
             method = RequestMethod.DELETE,
--- src/main/java/fi/tamk/beerbros/kaljakauppa/components/beer/BeerRepository.java
+++ src/main/java/fi/tamk/beerbros/kaljakauppa/components/beer/BeerRepository.java
@@ -2,6 +2,7 @@ package fi.tamk.beerbros.kaljakauppa.components.beer;
 
 import fi.tamk.beerbros.kaljakauppa.components.beertype.BeerType;
 import fi.tamk.beerbros.kaljakauppa.components.country.Country;
+import fi.tamk.beerbros.kaljakauppa.components.manufacturer.Manufacturer;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,4 +11,14 @@ public interface BeerRepository extends CrudRepository<Beer, Integer>{
     public List<Beer> findByCountry(Country country);
     
     public List<Beer> findByBeerType(BeerType beerType);
+    
+    public List<Beer> findByManufacturer(Manufacturer manufacturer);
+    
+    public List<Beer> findByBeerTypeAndCountry(BeerType beerType, Country country);
+    
+    public List<Beer> findByBeerTypeAndManufacturer(BeerType beerType, Manufacturer manufacturer);
+    
+    public List<Beer> findByCountryAndManufacturer(Country country, Manufacturer manufacturer);
+    
+    public List<Beer> findByCountryAndManufacturerAndBeerType(Country country, Manufacturer manufacturer, BeerType beerType);
 }
